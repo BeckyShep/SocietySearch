28824ec7ae16aa4efe75e0ca1be17cfd621faf52
Becky Shepherd <2109364s@student.gla.ac.uk>
2022-03-04 00:52:38 +0000

    Use these start up files

--- SocietySearchProject/manage.py
+++ SocietySearchProject/manage.py
@@ -1,10 +1,12 @@
+#!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SocietySearch.settings')
+    """Run administrative tasks."""
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SocietySearchProject.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
---
+++ SocietySearchProject/societysearch/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SocietysearchConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'societysearch'
